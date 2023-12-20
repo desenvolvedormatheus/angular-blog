51e6b37442d8af5e780cf0e09c7bd798f9311801
Matheus Pereira de Souza <109758729+desenvolvedormatheus@users.noreply.github.com>
2023-12-20 08:57:42 -0300

    sombras dos cards, nome do site

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -9,31 +9,31 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ContentComponent implements OnInit {
 
-  id:string | null = '0'
-  cardTitle:string = ''
-  cardDescription:string = ''
-  photoCover:string = ''
-  pubDate:string = ''
-  author:string = ''
-  linkNoticia:string = ''
+  id: string | null = '0'
+  cardTitle: string = ''
+  cardDescription: string = ''
+  photoCover: string = ''
+  pubDate: string = ''
+  author: string = ''
+  linkNoticia: string = ''
 
-  constructor(private NoticeService: NoticeService, private route:ActivatedRoute) {
+  constructor(private NoticeService: NoticeService, private route: ActivatedRoute) {
 
   }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe( value =>
+    this.route.paramMap.subscribe(value =>
       this.id = value.get("id")
     )
 
     this.NoticeService.getNotices()
-    .subscribe((data) => {
-      this.cardTitle = data.items[this.id || 0].titulo,
-      this.cardDescription = (data.items[this.id || 0].introducao).split("."),
-      this.photoCover = 'https://agenciadenoticias.ibge.gov.br/' + JSON.parse(data.items[this.id || 0].imagens).image_intro,
-      this.pubDate = data.items[this.id || 0].data_publicacao,
-      this.author = data.items[this.id || 0].editorias
-      this.linkNoticia = data.items[this.id || 0].link
-    })
+      .subscribe((data) => {
+        this.cardTitle = data.items[this.id || 0].titulo,
+        this.cardDescription = (data.items[this.id || 0].introducao).split("."),
+        this.photoCover = 'https://agenciadenoticias.ibge.gov.br/' + JSON.parse(data.items[this.id || 0].imagens).image_intro,
+        this.pubDate = data.items[this.id || 0].data_publicacao,
+        this.author = data.items[this.id || 0].editorias
+        this.linkNoticia = data.items[this.id || 0].link
+      })
   }
 }
