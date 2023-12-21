d9e63a488eeeb7b9600d30bd4cd0874042d9e46f
Matheus Pereira de Souza <109758729+desenvolvedormatheus@users.noreply.github.com>
2023-12-21 15:29:09 -0300

    adição do front de btn noturno

---
+++ src/app/components/btn-noturno/btn-noturno.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { NoticeService } from 'src/app/notice.service'
+
+@Component({
+  selector: 'app-btn-noturno',
+  templateUrl: './btn-noturno.component.html',
+  styleUrls: ['./btn-noturno.component.css']
+})
+export class btnNoturnoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
